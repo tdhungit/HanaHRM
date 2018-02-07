57a012b439886d0cc79964eda20d4dc550c367bd
JackyHome <jacky@youaddon.com>
2018-02-07 09:26:50 +0700

    fixed header

--- app/ui/default/components/Header/Header.js
+++ app/ui/default/components/Header/Header.js
@@ -82,25 +82,15 @@ export default class Header extends Component {
                                 <span className="d-md-down-none">admin</span>
                             </DropdownToggle>
                             <DropdownMenu right className={this.state.dropdownOpen ? 'show' : ''}>
-                                <DropdownItem header tag="div"
-                                              className="text-center"><strong>Account</strong></DropdownItem>
-                                <DropdownItem><i className="fa fa-bell-o"></i> Updates<Badge
-                                    color="info">42</Badge></DropdownItem>
-                                <DropdownItem><i className="fa fa-envelope-o"></i> Messages<Badge
-                                    color="success">42</Badge></DropdownItem>
-                                <DropdownItem><i className="fa fa-tasks"></i> Tasks<Badge
-                                    color="danger">42</Badge></DropdownItem>
-                                <DropdownItem><i className="fa fa-comments"></i> Comments<Badge
-                                    color="warning">42</Badge></DropdownItem>
-                                <DropdownItem header tag="div"
-                                              className="text-center"><strong>Settings</strong></DropdownItem>
+                                <DropdownItem header tag="div" className="text-center"><strong>Account</strong></DropdownItem>
+                                <DropdownItem><i className="fa fa-bell-o"></i> Updates<Badge color="info">42</Badge></DropdownItem>
+                                <DropdownItem><i className="fa fa-envelope-o"></i> Messages<Badge color="success">42</Badge></DropdownItem>
+                                <DropdownItem><i className="fa fa-tasks"></i> Tasks<Badge color="danger">42</Badge></DropdownItem>
+                                <DropdownItem><i className="fa fa-comments"></i> Comments<Badge color="warning">42</Badge></DropdownItem>
+                                <DropdownItem header tag="div" className="text-center"><strong>Settings</strong></DropdownItem>
                                 <DropdownItem><i className="fa fa-user"></i> Profile</DropdownItem>
                                 <DropdownItem><i className="fa fa-wrench"></i> Settings</DropdownItem>
-                                <DropdownItem><i className="fa fa-usd"></i> Payments<Badge color="secondary">42</Badge></DropdownItem>
-                                <DropdownItem><i className="fa fa-file"></i> Projects<Badge
-                                    color="primary">42</Badge></DropdownItem>
-                                <DropdownItem divider/>
-                                <DropdownItem><i className="fa fa-shield"></i> Lock Account</DropdownItem>
+                                {/*<DropdownItem divider/>*/}
                                 <DropdownItem><i className="fa fa-lock"></i> Logout</DropdownItem>
                             </DropdownMenu>
                         </Dropdown>
